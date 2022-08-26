f6c35cda75037f706f5efe21b2f56694c71f2c44
DESKTOP-BDQTBCG\Jannik-ThinkPad <jannik_walter@online.de>
2022-08-26 23:49:17 +0200

    config test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as data from '../assets/config.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'example-for-docker-deployment';
+  config = [data]
 }
