61015f64cd097752b3f57e5e6a23331188a1d729
JaWa3107 <70014344+JaWa3107@users.noreply.github.com>
2022-08-26 21:47:38 +0200

    Update app.component.ts
    addded: ansible test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'example-for-docker-deployment';
+  title = 'NO-LUCK-ALL-HUSTLE';
 }
